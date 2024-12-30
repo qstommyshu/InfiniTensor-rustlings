0027e8852e25ece6871004efcdc9612da53c1aa0
qstommyshu <qstommyshu@gmail.com>
2024-12-29 21:26:59 -0500

    Fix compiler errors and improve function implementations in string and module exercises

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,16 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.width = width;
+                self.height = height
+            }
+            Message::Move(p) => self.position = p,
+            Message::Echo(s) => self.message = s,
+            Message::ChangeColor(r, g, b) => self.color = (r, g, b),
+            Message::Quit => self.quit = true,
+        }
     }
 }
 
